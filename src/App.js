6ff8aa48d12599afd4e723e370ec980cb3b40911
hitagi777 <beqasirbila@mail.ru>
2019-08-19 00:58:01 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import './App.css';
+import './homepage.styles.scss';
+import HomePage from './homepage.component';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hello world
+      <div>
+        <HomePage />
+
       </div>
     );
   }
